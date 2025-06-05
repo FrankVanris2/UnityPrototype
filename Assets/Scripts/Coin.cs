90e56078fd3b459b57befe8b38e203d81ff768c4
FrankCVanris <frankcvanris@gmail.com>
2025-06-05 11:49:34 -0700

    Added a fun spin mechanic to the coin to add some pizzaz

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -5,10 +5,19 @@
  * When a player collides with a coin, it is collected and destroyed.
  */
 
+using System.Security.Cryptography;
+using System.Threading;
 using UnityEngine;
 
 public class Coin : MonoBehaviour
 {
+    [SerializeField] float spinSpeed = 90f;
+
+    void Update()
+    {
+        // Rotate the coin around it's Y-axis
+        transform.Rotate(0, spinSpeed * Time.deltaTime, 0);
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
